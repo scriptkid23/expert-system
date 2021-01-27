f486efc236a107e64946a46aefa4189d535dff9a
HoanDo <sieunhannhenkutesn@gmail.com>
2021-01-27 20:48:11 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import icon from '../assets/icon.svg';
+import './App.global.css';
+
+export default function App() {
+  return (
+    <div>
+      <p>Halo</p>
+    </div>
+  );
+}
